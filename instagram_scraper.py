84d1e13d659f840d92871bb5f0647e85dc91f9ae
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2016-11-22 19:10:13 -0500

    Completed email extraction

--- instagram_scraper.py
+++ instagram_scraper.py
@@ -5,9 +5,22 @@ November 2016
 '''
 from bs4 import BeautifulSoup
 import urllib2
+import re
 
 url = "http://www.pythonforbeginners.com"
 html_content = urllib2.urlopen(url).read()
 
+test_html = open('test_html.html', 'r')
+html_content = test_html.read()
+
 soup = BeautifulSoup(html_content, 'html.parser')
-print soup.prettify()
+bios = soup.findAll("div", { "class" : "uLBox-bio" })
+
+emails = []
+
+for bio in bios:
+	match = re.search(r'[\w\.-]+@[\w\.-]+', str(bio))
+	email = match.group(0)
+	emails.append(email)
+
+print(emails)
